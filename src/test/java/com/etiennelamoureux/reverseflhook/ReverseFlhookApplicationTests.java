4deecb7c6341c1e6ff9cd9e236179b45c426809f
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2021-01-22 11:14:48 -0500

    Bootstrap project

---
+++ src/test/java/com/etiennelamoureux/reverseflhook/ReverseFlhookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etiennelamoureux.reverseflhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverseFlhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
