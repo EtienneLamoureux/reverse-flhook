b1d697b10db0560684a153f201d33e0912b63ea1
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2021-01-26 11:43:21 -0500

    Successfully reimplement the generate ID method

--- src/main/java/com/etiennelamoureux/reverseflhook/utils/IdUtil.java
+++ src/main/java/com/etiennelamoureux/reverseflhook/utils/IdUtil.java
@@ -1,13 +1,16 @@
 package com.etiennelamoureux.reverseflhook.utils;
 
+import java.nio.charset.Charset;
+
 /**
  * @see <a href=
- *      "https://github.com/DiscoveryGC/FLHook/blob/3817c575c9584fd3c7f572af9d2c0ac0f4bf62eb/Plugins/Public/playercntl_plugin/setup_src/FLUtility.cs#L111">Reference</a>
+ *      "https://github.com/DiscoveryGC/FLHook/blob/3817c575c9584fd3c7f572af9d2c0ac0f4bf62eb/Plugins/Public/playercntl_plugin/setup_src/FLUtility.cs#L111">Original
+ *      algorithm</a>
  */
 public class IdUtil {
   private static final int FLHASH_POLYNOMIAL = 0xA001;
-  private static final int LOGICAL_BITS = 32;
-  private static final int PHYSICAL_BITS = 30;
+  private static final int LOGICAL_BITS = 30;
+  private static final int PHYSICAL_BITS = 32;
   private static final int[] HASHING_ARRAY = new int[256];
   static {
     for (int i = 0; i < 256; i++) {
@@ -23,14 +26,14 @@ public class IdUtil {
 
   public static int generate(String string) {
     int id = 0;
-    byte[] bytes = string.toLowerCase().getBytes();
+    byte[] bytes = string.toLowerCase().getBytes(Charset.forName("ASCII"));
 
     for (int i = 0; i < string.length(); i++) {
       id = (id >> 8) ^ HASHING_ARRAY[Byte.toUnsignedInt((byte) (id ^ bytes[i]))];
     }
 
     id = (id >> 24) | ((id >> 8) & 0x0000FF00) | ((id << 8) & 0x00FF0000) | (id << 24);
-    id = (id >> (PHYSICAL_BITS - LOGICAL_BITS)) | 0x80000000;
+    id = (id >>> (PHYSICAL_BITS - LOGICAL_BITS)) | 0x80000000;
 
     return id;
   }
--- src/test/java/com/etiennelamoureux/reverseflhook/utils/IdUtilTest.java
+++ src/test/java/com/etiennelamoureux/reverseflhook/utils/IdUtilTest.java
@@ -6,11 +6,16 @@ import org.junit.jupiter.api.Test;
 
 public class IdUtilTest {
   @Test
-  public void givenKnownNicknamesWhenConvertingThenGetKnownIds() {
-    assertEquals(-11193, IdUtil.generate("st01_to_st03_hole"));
-    assertEquals(27714, IdUtil.generate("st02_to_st01_hole"));
-    assertEquals(17474, IdUtil.generate("st03_to_st01_hole"));
-    assertEquals(21761, IdUtil.generate("li05_to_li01"));
-    assertEquals(-16127, IdUtil.generate("li01_to_li05"));
+  public void givenOriginalExamplesWhenConvertingThenGetKnownIds() {
+    assertEquals(2926433351l, Integer.toUnsignedLong(IdUtil.generate("st01_to_st03_hole")));
+    assertEquals(2460445762l, Integer.toUnsignedLong(IdUtil.generate("st02_to_st01_hole")));
+    assertEquals(2263303234l, Integer.toUnsignedLong(IdUtil.generate("st03_to_st01_hole")));
+    assertEquals(2284213505l, Integer.toUnsignedLong(IdUtil.generate("li05_to_li01")));
+    assertEquals(2293678337l, Integer.toUnsignedLong(IdUtil.generate("li01_to_li05")));
+  }
+
+  @Test
+  public void givenTau37WhenConvertingThenGetKnownId() {
+    assertEquals(-1146109436, IdUtil.generate("ew01"));
   }
 }
