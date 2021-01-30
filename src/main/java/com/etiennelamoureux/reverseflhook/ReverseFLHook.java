24eb5f0d2895019d0d78f1af9c9f96ccc9d767eb
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2021-01-30 16:43:36 -0500

    Implement the copy flag

--- src/main/java/com/etiennelamoureux/reverseflhook/ReverseFLHook.java
+++ src/main/java/com/etiennelamoureux/reverseflhook/ReverseFLHook.java
@@ -1,13 +1,13 @@
 package com.etiennelamoureux.reverseflhook;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ReverseFLHook {
 
   public static void main(String[] args) {
-    SpringApplication.run(ReverseFLHook.class, args);
+    new SpringApplicationBuilder(ReverseFLHook.class).headless(false).run(args);
   }
 
 }
